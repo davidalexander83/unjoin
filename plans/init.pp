c255bf4d7426a887be95704407190114d58a4d44
David Alexander <david.alexander@puppet.com>
2021-04-12 02:55:20 +0000

    Initial commit

---
+++ plans/init.pp
@@ -0,0 +1,33 @@
+plan unjoin (
+  TargetSpec $targets
+) {
+  # Install the puppet-agent package and gather facts
+  $targets.apply_prep
+
+  # Apply Puppet code
+  $apply_results = apply($targets) {
+      dsc_computer { 'Unjoin domain':
+        dsc_name          => $facts['hostname'],
+        dsc_workgroupname => 'NotDomain',
+        dsc_credential    => {
+          'user'          => 'mydomain\\domain_join',
+          'password'      => Sensitive('Puppet4Life')
+        },
+      }
+  }
+  
+  # Print log messages from the report
+  $apply_results.each |$result| {
+    $result.report['logs'].each |$log| {
+      out::message("${log['level'].upcase}: ${log['message']}")
+    }
+  }
+
+  $reboot_results = run_task('reboot', 'targets' => $targets)
+
+  $reboot_results.each |$reboot_result| {
+    $reboot_result['logs'].each |$log| {
+      out::message("${log['level'].upcase}: ${log['message']}")
+    }
+  }
+}
